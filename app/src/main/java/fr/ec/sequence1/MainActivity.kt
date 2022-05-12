9a176fd8e0c51a74e971381ce5ea08e842de4ecd
Mohammed Boukadir <me@mboukadir.dev>
2022-05-12 11:34:22 +0200

    Display item list with recyclerview

--- app/src/main/java/fr/ec/sequence1/MainActivity.kt
+++ app/src/main/java/fr/ec/sequence1/MainActivity.kt
@@ -1,26 +1,81 @@
 package fr.ec.sequence1
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.VideoView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = findViewById<LinearLayout>(R.id.list)
+        val list = findViewById<RecyclerView>(R.id.list)
+        list.adapter = ItemAdapter(dataSet = provideDataSet())
+        list.layoutManager = LinearLayoutManager(this, VERTICAL, false)
 
-        repeat(10) { index ->
-            val item = layoutInflater.inflate(R.layout.item_layout,list,false)
-            val title = item.findViewById<TextView>(R.id.title)
-            val subtitle = item.findViewById<TextView>(R.id.subtitle)
+    }
 
-            title.text = "Titre $index"
-            subtitle.text = "Sous-titre $index"
-            list.addView(item)
 
+    class ItemAdapter(
+        private val dataSet: List<Item>
+    ) : RecyclerView.Adapter<ItemViewHolder>() {
+
+        override fun getItemCount(): Int = dataSet.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+            val itemView =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+            return ItemViewHolder(itemView = itemView)
+        }
+
+        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            holder.bind(item = dataSet[position])
+        }
+
+
+    }
+
+    fun provideDataSet(): List<Item> {
+        val result = mutableListOf<Item>()
+        repeat(1_000) { intex ->
+            val item = Item(
+                imageRes = R.mipmap.ic_launcher,
+                title = "Titre $intex",
+                subTitle = "Sous-Titre $intex",
+            )
+
+            result.add(item)
+        }
+        return result
+    }
+
+    data class Item(
+        val imageRes: Int,
+        val title: String,
+        val subTitle: String
+    )
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val image = itemView.findViewById<ImageView>(R.id.image)
+        private val title = itemView.findViewById<TextView>(R.id.title)
+        private val subtitle = itemView.findViewById<TextView>(R.id.subtitle)
+
+        fun bind(item: Item) {
+            title.text = item.title
+            subtitle.text = item.subTitle
+            image.setImageResource(item.imageRes)
         }
 
     }
