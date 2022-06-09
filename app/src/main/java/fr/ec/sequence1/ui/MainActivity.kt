d9ad57a3ded32c501251cd9dfb0e59789dfff774
Mohammed Boukadir <me@mboukadir.dev>
2022-06-09 10:36:07 +0200

    Display loader

--- app/src/main/java/fr/ec/sequence1/ui/MainActivity.kt
+++ app/src/main/java/fr/ec/sequence1/ui/MainActivity.kt
@@ -3,11 +3,14 @@ package fr.ec.sequence1.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import fr.ec.sequence1.R
 import fr.ec.sequence1.data.DataProvider
+import fr.ec.sequence1.data.Post
 import fr.ec.sequence1.ui.adapter.ItemAdapter
 import fr.ec.sequence1.ui.model.ListItem
 import kotlinx.coroutines.*
@@ -15,11 +18,13 @@ import kotlinx.coroutines.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var adapter: ItemAdapter
+    private lateinit var loader : ProgressBar
     private val dataProvider : DataProvider = DataProvider(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupRecyclerview()
+        loader = findViewById<ProgressBar>(R.id.loader)
         loadPosts()
     }
 
@@ -42,25 +47,35 @@ class MainActivity : AppCompatActivity() {
     private fun loadPosts() {
 
         mainActivityScope.launch {
+            loader.visibility = View.VISIBLE
 
-            Log.d("MainActivity", "Start")
-
-            val posts = dataProvider.getPosts()
-
-            Log.d("MainActivity", "End")
-
-            val items = withContext(Dispatchers.Default) {
-                 posts.map { post ->
-                    ListItem.Item(
-                        imageUrl = post.imageUrl,
-                        title = post.title,
-                        subTitle = post.subtitle
-                    )
+            runCatching {
+                getPosts()
+            }.fold(
+                onSuccess = { items ->
+                    adapter.display(items)
+                    loader.visibility = View.GONE
+                },
+                onFailure = {
+                    loader.visibility = View.GONE
+                    // displayErrorScreen
+                    Log.e("MainActivity", "Fails -> $it")
                 }
+            )
+
+        }
+    }
+
+    private suspend fun getPosts() : List<ListItem.Item>{
+        val posts = dataProvider.getPosts()
+      return  withContext(Dispatchers.Default) {
+            posts.map { post ->
+                ListItem.Item(
+                    imageUrl = post.imageUrl,
+                    title = post.title,
+                    subTitle = post.subtitle
+                )
             }
-
-            adapter.display(items)
-
         }
     }
 }
